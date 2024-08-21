5a45645bdee89bc46427b93802b4a09c537822d8
Jakob <codemonkey727@gmail.com>
2024-08-21 18:38:49 -0400

    add logic to play 1 full round of rock paper scissors
    i added the logic to play a full round, while doing so i realized
    my current logic for grabbing the computer input was making things
    more difficult so i changed that as well. previously it returned
    a number which represented rock paper or scissors but now i convert
    that number into a string first so it returns a string same as the
    human input.

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 let humanScore = 0, computerScore = 0;
 
 function getComputerChoice(){
-    return Math.floor(Math.random() * 3);
+    let random = Math.floor(Math.random() * 3);
+    switch (random){
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+        default:
+            return 'rock';
+    }
 }
 
 function getHumanChoice(){
-    return prompt("Choose your fighter! (Enter Rock, Paper, or Scissors)");
+    return prompt('Choose your fighter! (Enter Rock, Paper, or Scissors)');
 }
 
-console.log(getHumanChoice())
+function playRound(humanChoice = "", computerChoice){
+    let human = humanChoice.toLowerCase();
+    let computer = computerChoice; //0 = rock, 1 = paper, 2 = scissors
+    
+    if((human == 'rock' && computer == 'paper') 
+        || (human == 'paper' && computer == 'scissors') 
+        || (human == 'scissors' && computer == 'rock')){
+        console.log(`You Lose! ${computer} beats ${human}`);
+        computerScore++;
+    }
+
+    if((human == 'rock' && computer == 'scissors') 
+        || (human == 'paper' && computer == 'rock') 
+        || (human == 'scissors' && computer == 'paper')){
+        console.log(`You Win! ${human} beats ${computer}`);
+        humanScore++;
+    }
+
+    if(human == computer){
+        console.log(`DRAW`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
