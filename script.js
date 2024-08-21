896b6334d4b1e890232611af3caeaf96bd073fa1
Jakob <codemonkey727@gmail.com>
2024-08-21 18:04:34 -0400

    write function to get random number from 0-2
    we need to develop a way to get a random number from the computer
    so we have something to compete against in rock paper scissors
    this function returns a number between 0-2 and each number will
    represent the 3 choices in rock paper scissors

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+function getComputerChoice(){
+    return Math.floor(Math.random() * 3)
+}
+
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
